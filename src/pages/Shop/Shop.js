04f048452fc0c20a10d15116e02dd27ab1304d84
yeasinbinali <yeasinbinali@gmail.com>
2023-07-27 22:54:20 +0600

    image and products json added

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Shop = () => {
-    return (
-        <div>
-            <h2>Shop</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Shop</h2>
+    </div>
+  );
 };
 
 export default Shop;
